2e32e675f4395d5b71530b5ff4c1354e4a71ea87
Adrien <verblodung@gmail.com>
2017-12-19 15:45:00 -0500

    feat(card): adding basic card in center

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <div className="card">test</div>
     <Link to="/page-2/">Go to page 2</Link>
     |
     <Link to="/typography/">Go to page Typography</Link>
